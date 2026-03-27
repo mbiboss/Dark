1ac314123c9aa71310133dce3c9b2272d1ef18ae
MBI BOSS <122667057+mbiboss@users.noreply.github.com>
2026-03-27 14:23:28 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'mbi-dark-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './about.html',
+  './projects.html',
+  './contact.html',
+  './404.html',
+  './style.css',
+  './script.js',
+  './projects.json',
+  './logo.jpg',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
